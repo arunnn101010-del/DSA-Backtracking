650adfefb02f1a714c472837ea51b664bff71eff
Arun <arunnn101010@gmail.com>
2026-08-09 23:04:14 +0530

    Update Backtracking : binary watch : solution.py

--- Backtracking : binary watch : solution.py
+++ Backtracking : binary watch : solution.py
@@ -1,1 +1,25 @@
+# Promblem - binary search 
+# Approach - greedy 
+# Time and space complexity - 0(1) & 0(1) 
+# Leetcode and diffculty level - 401 & easy
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
 
+        vector<string> ans;
+
+        for(int h = 0; h < 12; h++) {
+
+            for(int m = 0; m < 60; m++) {
+
+                if(__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+
+                    ans.push_back(to_string(h) + ":" +
+                    (m < 10 ? "0" : "") + to_string(m));
+                }
+            }
+        }
+
+        return ans;
+    }
+};
