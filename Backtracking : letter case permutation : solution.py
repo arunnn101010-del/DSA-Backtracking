693c7fb216abc6126b07b25731ad0106c983e49f
Arun <arunnn101010@gmail.com>
2026-09-01 19:12:15 +0530

    Update Backtracking : letter case permutation : solution.py

--- Backtracking : letter case permutation : solution.py
+++ Backtracking : letter case permutation : solution.py
@@ -1,1 +1,34 @@
+# Promblem - letter case permutation 
+# Approach - backtracking 
+# Time and space complexity - 0(2^n * 2) & 0(n) 
+# Leetcode and diffculty level - 784 & easy 
+class Solution {
+public:
+    void solve(string& s, int i, vector<string>& ans) {
 
+        if(i == s.size()) {
+            ans.push_back(s);
+            return;
+        }
+
+        if(isalpha(s[i])) {
+
+            s[i] = tolower(s[i]);
+            solve(s, i + 1, ans);
+
+            s[i] = toupper(s[i]);
+            solve(s, i + 1, ans);
+        }
+        else {
+            solve(s, i + 1, ans);
+        }
+    }
+
+    vector<string> letterCasePermutation(string s) {
+
+        vector<string> ans;
+        solve(s, 0, ans);
+
+        return ans;
+    }
+};
