428af192e3b3e8435159010a608c2eb8baa37613
Arun <arunnn101010@gmail.com>
2026-09-01 19:43:52 +0530

    Add backtracking solution for letter tile possibilities
    Implemented backtracking solution for counting letter tile possibilities based on given tiles.

--- Backtracking : letter tile possibilities : solution.py
+++ Backtracking : letter tile possibilities : solution.py
@@ -1,1 +1,30 @@
+# promblem - letter tile possbilities 
+# Approach - backtracking 
+# Time and space complexity - 0(n) & 0(n) 
+# Diffculty level and leetcode - easy & 1079 
+class Solution {
+public:
+    int solve(vector<int>& count) {
+        int ans = 0;
 
+        for(int i = 0; i < 26; i++) {
+            if(count[i] == 0) 
+                continue;
+            count[i]--;
+            ans++;
+
+            ans += solve(count);
+
+            count[i]++;
+        }
+        return ans;
+    }
+    int numTilePossibilities(string tiles) {
+        vector<int> count(26);
+
+        for(char c : tiles) {
+            count[c - 'A']++;
+        }
+        return solve(count);
+    }
+};
