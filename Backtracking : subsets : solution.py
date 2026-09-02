55e14d20559737943f437a51f0e3614e3f756d58
Arun <arunnn101010@gmail.com>
2026-09-02 21:48:36 +0530

    Add backtracking solution for generating subsets
    Implemented backtracking approach to generate all subsets of a given set of numbers.

--- Backtracking : subsets : solution.py
+++ Backtracking : subsets : solution.py
@@ -1,1 +1,32 @@
+# promblem - subsets 
+# Approach - backtracking 
+# Time and space complexity - 0(2 ^ n * n) & 0(n) 
+# Leetcode and diffculty level - 78 & medium 
+class Solution {
+public:
+    void solve(vector<int>& nums, int i, vector<int>& curr,
+               vector<vector<int>>& ans) {
 
+        if(i == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        solve(nums, i + 1, curr, ans);
+
+        curr.pop_back();
+
+        solve(nums, i + 1, curr, ans);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        solve(nums, 0, curr, ans);
+
+        return ans;
+    }
+};
